d925d7cc80b45a9b96797a4e14ab890779e9ae98
Diego Dalairac <ddalairac@santandertecnologia.com.ar>
2020-10-17 10:47:09 -0300

    nav styles

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,15 @@
 
 export const environment = {
   production: false,
-  firebaseConfig:{}
+  firebaseConfig: {
+    apiKey: "AIzaSyBd0bqE7fIdblG486RVsfJ1MttdgBNs9KE",
+    authDomain: "lab4-tp-clinic.firebaseapp.com",
+    databaseURL: "https://lab4-tp-clinic.firebaseio.com",
+    projectId: "lab4-tp-clinic",
+    storageBucket: "lab4-tp-clinic.appspot.com",
+    messagingSenderId: "834788398553",
+    appId: "1:834788398553:web:f49828546eb00a0f0e35c8"
+  }
 };
 
 /*
